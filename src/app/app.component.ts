4bf83667b6437c61ddc2ca647b6d5813325713b3
johnlauren <aplusview@gmail.com>
2024-07-25 14:34:17 -0400

    navibar updated. adding privacy policy page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'h Sharp';
+  footertext = 'Copyright ' + (new Date()).getFullYear() + ' - HSharp';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PrivatePolicyComponent } from './about/private-policy/private-policy.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'privatepolicy', component: PrivatePolicyComponent},
+    { path: '', redirectTo: 'Welcome', pathMatch: 'full'},
+    { path: '**', component: WelcomeComponent}
+];
