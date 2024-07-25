d240a941ce83a68d373df62673a2f436da7ac58c
johnlauren <aplusview@gmail.com>
2024-07-25 14:42:45 -0400

    add about and home folder. add assets folder. update favicon.ico

---
+++ src/app/about/private-policy/private-policy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-private-policy',
+  standalone: true,
+  imports: [],
+  templateUrl: './private-policy.component.html',
+  styleUrl: './private-policy.component.css'
+})
+export class PrivatePolicyComponent {
+
+}
